b145ea5fe707b376ac6a8815f393ad3bf704130b
Ishmael Obeng <electrons500@gmail.com>
2024-02-01 11:36:07 +0000

    first commit

---
+++ LoggingApiWithSerilog/Controllers/WeatherForecastController.cs
@@ -0,0 +1,50 @@
+using Microsoft.AspNetCore.Mvc;
+using Serilog;
+
+namespace LoggingApiWithSerilog.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            try
+            {
+                
+                var result = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Date = DateTime.Now.AddDays(index),
+                    TemperatureC = Random.Shared.Next(-20, 55),
+                    Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                })
+                .ToArray();
+
+                Log.Information("WeatherForcast => {@result}", result);
+            
+                return result;
+
+            }
+            catch (Exception ex)
+            {
+                
+                Log.Error(ex.Message,"Error from WeatherForecast Get");
+               return new List<WeatherForecast>();
+            }
+
+        }
+    }
+}
